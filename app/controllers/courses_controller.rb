bc11d7052950b146fdca8c8c899df0c8026b0879
ashish2705 <ashishjaiswal.iitd@gmail.com>
2020-06-07 00:01:46 +0530

    ch

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = 'ruby'
+  	@search_term = params[:looking_for] 
   	@crs = Coursera.for(@search_term)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'greeter/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'greeter/goodbye'
+
+  root 'courses#index'
 end
